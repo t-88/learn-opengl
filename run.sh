7469158a2beb1542c794ef4ea514a36de4007cf1
t-88 <temptaki5002@gmail.com>
2023-12-01 15:54:40 +0100

    create utils and update run to work with it

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 
 set -eux
 gcc -c  ./lib/glad/src/glad.c -o build/glad.o    -I./lib/glad/include/  
-g++ -c  main.cpp          -o ./build/main.o  -I./lib/glad/include/ -I./include/ 
-g++ -o build/main build/glad.o build/main.o  `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs opengl`
+g++ -c  main.cpp          -o ./build/main.o    -I./include -I./lib/glad/include/ 
+g++  -o build/main build/glad.o build/main.o src/*.cpp    -I./include `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs opengl`
 ./build/main
