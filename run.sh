ee722c2b251a4ad50a5f4facce9ec35b7cd9e295
t-88 <temptaki5002@gmail.com>
2023-12-02 14:28:25 +0100

    add glm lib

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+HEADERS="-I./include -I./lib/ -I./lib/glad/include/"
 
 set -eux
 gcc -c  ./lib/glad/src/glad.c -o build/glad.o    -I./lib/glad/include/  
-g++ -c  main.cpp          -o ./build/main.o    -I./include -I./lib/glad/include/ 
-g++  -o build/main build/glad.o build/main.o src/*.cpp    -I./include -I./lib/glad/include/  `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs opengl`
+g++ -c  main.cpp          -o ./build/main.o  $HEADERS
+g++  -o build/main build/glad.o build/main.o src/*.cpp   $HEADERS  `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs opengl`
 ./build/main
