7416fe1f0fec418735a7c6ba68ceb7e9b20b2666
t-88 <temptaki5002@gmail.com>
2023-12-06 14:52:48 +0100

    implement simple lighting

--- main.cpp
+++ main.cpp
@@ -46,6 +46,8 @@ typedef struct camera_t {
 } camera_t;
 static camera_t camera;
 
+
+
 void on_window_resize(GLFWwindow* window, int w, int h) {
     // do nothing window is not resizable
 }
@@ -104,52 +106,47 @@ int main() {
 
 
     float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 
+         0.5f, -0.5f, -0.5f, 
+         0.5f,  0.5f, -0.5f, 
+         0.5f,  0.5f, -0.5f, 
+        -0.5f,  0.5f, -0.5f, 
+        -0.5f, -0.5f, -0.5f, 
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 
+         0.5f, -0.5f,  0.5f, 
+         0.5f,  0.5f,  0.5f, 
+         0.5f,  0.5f,  0.5f, 
+        -0.5f,  0.5f,  0.5f, 
+        -0.5f, -0.5f,  0.5f, 
 
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 
+        -0.5f,  0.5f, -0.5f, 
+        -0.5f, -0.5f, -0.5f, 
+        -0.5f, -0.5f, -0.5f, 
+        -0.5f, -0.5f,  0.5f, 
+        -0.5f,  0.5f,  0.5f, 
 
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  
+         0.5f,  0.5f, -0.5f,  
+         0.5f, -0.5f, -0.5f,  
+         0.5f, -0.5f, -0.5f,  
+         0.5f, -0.5f,  0.5f,  
+         0.5f,  0.5f,  0.5f,  
 
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
-
-    uint32_t indices[] = {
-        0 , 1 , 2,
-        1 , 2 , 3,
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
     };
 
 
@@ -157,55 +154,27 @@ int main() {
     glGenVertexArrays(1,&vao);
     uint32_t vbo; 
     glGenBuffers(1,&vbo);
-    uint32_t ebo;
-    glGenBuffers(1,&ebo);
 
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER,vbo);
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float) * 5,0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float) * 3,0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1,2,GL_FLOAT,GL_FALSE,sizeof(float) * 5,(void*) (sizeof(float) * 3));
-    glEnableVertexAttribArray(1);
-
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
     glBindBuffer(GL_ARRAY_BUFFER,0);
     glBindVertexArray(0);
 
 
-    Shader shader_prog("shader.vert","shader.frag");
-
-
-    int tex_w,tex_h;
-    stbi_set_flip_vertically_on_load(true);
-    uint8_t* tex_data = stbi_load("./assets/container.jpg",&tex_w,&tex_h,0,3);
-    if(!tex_data) {
-        ERROR("failed to load img");
-        glfwTerminate();
-        exit(-1);
-    }
-
-    uint32_t tex_id;
-    glGenTextures(1,&tex_id);
-    glBindTexture(GL_TEXTURE_2D,tex_id);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_R,GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,tex_w,tex_h,0,GL_RGB,GL_UNSIGNED_BYTE,tex_data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    stbi_image_free(tex_data);
+    Shader shader_prog_obj("shader.vert","shader.frag");
+    Shader shader_prog_light("shader.vert","shader_light.frag");
 
 
     camera = (camera_t){
-        glm::vec3(0.,0.,5.),
-        glm::vec3(0.,0.,-1.),
+
+        glm::vec3(0.,0.,4.),
+        glm::vec3(0.,0.,0.),
         glm::vec3(0.,1.,0.),
     };
 
@@ -214,23 +183,35 @@ int main() {
     glm::mat4 view = glm::mat4(1.);
     glm::mat4 proj = glm::perspective(glm::radians(45.f),(float)WIDTH/HEIGHT,0.1f,100.f);
 
-    shader_prog.enable();
+    shader_prog_obj.enable();
+    shader_prog_obj.set_mat4x4("view",glm::value_ptr(view));
+    shader_prog_obj.set_mat4x4("proj",glm::value_ptr(proj));
+    shader_prog_obj.set_mat4x4("model",glm::value_ptr(model));
 
-    shader_prog.set_mat4x4("view",glm::value_ptr(view));
-    shader_prog.set_mat4x4("proj",glm::value_ptr(proj));
-    shader_prog.set_mat4x4("model",glm::value_ptr(model));
+    shader_prog_light.enable();
+    shader_prog_light.set_mat4x4("view",glm::value_ptr(view));
+    shader_prog_light.set_mat4x4("proj",glm::value_ptr(proj));
+    shader_prog_light.set_mat4x4("model",glm::value_ptr(model));
 
     
 
-    glm::vec3 center_cube_pos   = glm::vec3(0.f,0.f,-2.f);
-    float center_cube_rotation   = 0.f;
+    glm::mat4 model_obj = glm::mat4(1.);
+    model_obj = glm::translate(model_obj,glm::vec3(1,0.,-0.5));
 
-    glm::vec3 rotating_cube_pos   = glm::vec3(0.f,0.f,-4.f);
-    float rotating_cube_rotation   = 0.f;
+    glm::mat4 model_light = glm::mat4(1.);
+    model_light = glm::translate(model_light,glm::vec3(-1.,0.,0.5));
+    model_light = glm::scale(model_light,glm::vec3(0.5));
 
 
+    std::vector<float> light_color = {1.,0.25,1.};
+    shader_prog_light.enable();
+    shader_prog_light.set_vec3("light_color",light_color);
 
-  
+    std::vector<float> obj_color = {0.25,0.3,1.};
+    shader_prog_obj.enable();
+    shader_prog_obj.set_vec3("obj_color",obj_color);
+    shader_prog_obj.set_vec3("light_color",light_color);
+
 
 
     float t;
@@ -240,67 +221,50 @@ int main() {
     glfwSetInputMode(window,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, mouse_callback);  
     while (!glfwWindowShouldClose(window)) {
-        
+        // dt
         t = glfwGetTime();
         dt = t - last_time;
         last_time = t;
 
-
-
+        // input
         glfwPollEvents();
-
         if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window,true);
         }
-
-
         camera.process_input(window);
 
-
-
-        shader_prog.enable();
 
         view = glm::lookAt(
             camera.pos,
             camera.pos + camera.front,
             camera.up
         );
-
         glm::vec3 dir;
         dir.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
         dir.y = sin(glm::radians(pitch)) ;
         dir.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
         camera.front = glm::normalize(dir);
 
-        shader_prog.set_mat4x4("view",glm::value_ptr(view));
         
         
-	
-	    glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D,tex_id);
 
+
+     
+        
+
+
+        shader_prog_light.enable();
+        shader_prog_light.set_mat4x4("view",glm::value_ptr(view));
+        shader_prog_light.set_mat4x4("model",glm::value_ptr(model_light));
         glBindVertexArray(vao);
-        
-
-        center_cube_rotation += 2;
-        glm::mat4 model = glm::mat4(1.f);
-        model = glm::translate(model,center_cube_pos);
-        model = glm::rotate(model,glm::radians(center_cube_rotation),glm::vec3(1.f,1.f,1.f));
-        shader_prog.set_mat4x4("model",glm::value_ptr(model));
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-        rotating_cube_rotation += 1; 
-        model = glm::mat4(1.f);
-        model = glm::translate(model,rotating_cube_pos);
-        model = glm::rotate(model,glm::radians(rotating_cube_rotation),glm::vec3(1.f,0.f,0.f));
-        model = glm::rotate(model,glm::radians(rotating_cube_rotation / 2),glm::vec3(0.f,1.f,0.f));
-        model = glm::rotate(model,glm::radians(rotating_cube_rotation / 4),glm::vec3(0.f,0.f,1.f));
-        model = glm::translate(model,glm::vec3(0.f,0.,3.));
-        shader_prog.set_mat4x4("model",glm::value_ptr(model));
+        shader_prog_obj.enable();
+        shader_prog_obj.set_mat4x4("view",glm::value_ptr(view));
+        shader_prog_obj.set_mat4x4("model",glm::value_ptr(model_obj));
+        glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
 
 
 
@@ -309,7 +273,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
     
-    shader_prog.free();
+    shader_prog_obj.free();
 
     glfwDestroyWindow(window);
     glfwTerminate();
